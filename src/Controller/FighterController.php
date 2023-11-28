47d9fced2924b8e5ed08deaec3738f9155b4abe2
WilsonSan5 <wilson.san57@gmail.com>
2023-11-28 22:22:26 +0100

    added route for request fighters, added request page index

--- src/Controller/FighterController.php
+++ src/Controller/FighterController.php
@@ -21,9 +21,16 @@ class FighterController extends AbstractController
     #[Route('/', name: 'app_fighter_index', methods: ['GET'])]
     public function index(FighterRepository $fighterRepository): Response
     {
-        $newFighters = $fighterRepository->findBy(['is_valid' => false]);
         return $this->render('fighter/index.html.twig', [
-            'fighters' => $newFighters,
+            'fighters' => $fighterRepository->findBy(['is_valid' => true])
+        ]);
+    }
+
+    #[Route('/requestedFighters', name: 'app_fighter_request', methods: ['GET'])]
+    public function indexRequestFighter(FighterRepository $fighterRepository): Response
+    {
+        return $this->render('fighter/index_request_fighters.html.twig', [
+            'newFighters' => $fighterRepository->findBy(['is_valid' => false]),
         ]);
     }
 
@@ -36,7 +43,7 @@ class FighterController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             // Generating new fighter's fights with all other fighter
-            $allFighters = $fighterRepository->findAll();
+            $allFighters = $fighterRepository->findBy(['is_valid' => true]);
             foreach ($allFighters as $fighter_2) {
                 $newFight = new Fight();
                 $newFight->addFighter($fighter);
@@ -78,12 +85,9 @@ class FighterController extends AbstractController
     #[Route('/{id}', name: 'app_fighter_show', methods: ['GET'])]
     public function show(Fighter $fighter, FightRepository $fightRepository): Response
     {
-
-
         return $this->render('fighter/show.html.twig', [
             'fighter' => $fighter,
             'numberOfFights' => count($fighter->getFights()),
-
         ]);
     }
 
@@ -96,7 +100,7 @@ class FighterController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_fighter_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_fighter_show', ['id' => $fighter->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('fighter/edit.html.twig', [
@@ -104,6 +108,47 @@ class FighterController extends AbstractController
             'form' => $form,
         ]);
     }
+
+    #[Route('/{id}/generateFight', name: 'app_fighter_generate_fight')]
+    public function generateFight(Fighter $fighter, FighterRepository $fighterRepository, EntityManagerInterface $entityManager): Response
+    {
+        // Generating new fighter's fights with all other fighter
+        if ($fighter->isIsValid() === true && $fighter->getStrength() > 0) {
+            $allFighters = $fighterRepository->find(['is_valid' => true]);
+            foreach ($allFighters as $fighter_2) {
+                $newFight = new Fight();
+                $newFight->addFighter($fighter);
+                $newFight->addFighter($fighter_2);
+
+                // Setting isBalanced : If the strength gap between 2 characters is less than 2, the fight is balanced.
+                if (abs($fighter->getStrength() - $fighter_2->getStrength()) < 2) {
+                    $newFight->setIsBalanced(true);
+                } else
+                    $newFight->setIsBalanced(false);
+
+                // Setting the votes for the new fighter
+                $newFighter_vote = new Vote;
+                $newFighter_vote->setNumberOfVotes(0);
+                $newFighter_vote->setFighter($fighter);
+                $newFight->addVote($newFighter_vote);
+
+                // Setting the votes for fighter 2
+                $fighter_2_votes = new Vote;
+                $fighter_2_votes->setNumberOfVotes(0);
+                $fighter_2_votes->setFighter($fighter_2);
+                $newFight->addVote($fighter_2_votes);
+
+                $entityManager->persist($newFighter_vote);
+                $entityManager->persist($fighter_2_votes);
+                $entityManager->persist($newFight);
+            }
+            $entityManager->persist($fighter);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_fighter_show', ['id' => $fighter->getId()], Response::HTTP_SEE_OTHER);
+    }
+
 
     #[Route('/{id}', name: 'app_fighter_delete', methods: ['POST'])]
     public function delete(Request $request, Fighter $fighter, EntityManagerInterface $entityManager): Response
