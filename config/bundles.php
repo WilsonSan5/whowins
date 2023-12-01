73f549e112d5d4d86c38e399091ce92cfde7d76f
WilsonSan5 <wilson.san57@gmail.com>
2023-12-01 20:05:51 +0100

    add nelmio cors

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
